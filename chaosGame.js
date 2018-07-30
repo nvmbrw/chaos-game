fb1e80caa2f0e9d89670028c0fe8047d1da290f3
november <nnwilliams8@gmail.com>
2018-07-30 09:02:10 -0400

    made a simple Point class to eliminate need for manually accessing array indexes

--- chaosGame.js
+++ chaosGame.js
@@ -2,32 +2,44 @@ let cx = document.querySelector('canvas').getContext('2d')
 
 cx.fillStyle = 'red'
 
-const pixel = (x, y) => cx.fillRect(x, y, 1, 1)
-const bigDot = (x, y) => cx.fillRect(x, y, 4, 4)
-
-const A = [450, 190],
-  B = [150, 710],
-  C = [750, 710]
-
-bigDot(A[0], A[1])
-bigDot(B[0], B[1])
-bigDot(C[0], C[1])
-
-const vertices = [A, B, C]
-
-const midpoint = (a, b) => [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2]
-
-const pickVertex = () => vertices[Math.round(Math.random() * 2)]
-console.log(pickVertex())
-const getNewPos = oldPos => midpoint(oldPos, pickVertex())
-
-const playGame = iterations => {
-  const startVertex = pickVertex()
-  let currentPos = startVertex.slice()
-  for (let i = 0; i < iterations; i++) {
-    currentPos = getNewPos(currentPos)
-    pixel(currentPos[0], currentPos[1])
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+  midpointTo(a) {
+    return Point.midpoint(this, a)
+  }
+  static midpoint(a, b) {
+    return new Point(Math.round((a.x + b.x) / 2), Math.round((a.y + b.y) / 2))
   }
 }
 
-playGame(10000)
+const pixel = point => cx.fillRect(point.x, point.y, 1, 1)
+const bigDot = point => cx.fillRect(point.x, point.y, 1, 1)
+
+const A = new Point(450, 190),
+  B = new Point(150, 710),
+  C = new Point(750, 710)
+
+bigDot(A)
+bigDot(B)
+bigDot(C)
+
+const vertices = [A, B, C]
+
+const pickVertex = () =>
+  vertices[Math.round(Math.random() * (vertices.length - 1))]
+
+const getNewPoint = oldPoint => oldPoint.midpointTo(pickVertex())
+
+const playGame = iterations => {
+  const startVertex = pickVertex()
+  let currentPos = startVertex
+  for (let i = 0; i < iterations; i++) {
+    currentPos = getNewPoint(currentPos)
+    pixel(currentPos)
+  }
+}
+
+playGame(100000)
